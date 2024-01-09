29b638b83f4f7f79893e56249252338a00569c82
Raffaela Loffredo <raffaloffredo@gmail.com>
2024-01-09 17:11:31 +0000

    Created fact table "orders"

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -1,3 +1,14 @@
 with customers as (
-    select * from dim_customers
+    select * from {{ ref('dim_customers')}}
 )
+, orders as (
+    select * from {{ ref('stg_orders')}}
+)
+
+select 
+    customers.sk_customer
+    , orders.*
+from orders
+left join customers
+    on orders.customer_id = customers.customer_id
+    
