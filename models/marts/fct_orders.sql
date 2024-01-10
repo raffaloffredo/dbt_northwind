1aa9450f0ef375b471384c5a30e72dfc50826bd9
Raffaela Loffredo <raffaloffredo@gmail.com>
2024-01-10 20:33:18 +0000

    Added singular test revenue

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -5,11 +5,9 @@ with
     , orders as (
         select * from {{ ref('dim_orders')}}
     )
-
 select 
     customers.sk_customers
     , orders.*
 from orders
 left join customers
     on orders.customer_id = customers.customer_id
-    
